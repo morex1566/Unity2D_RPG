c653f263552fbf0a4cf3ab49593045c497af445d
Jiwon <morex1566@gmail.com>
2025-06-21 02:28:56 +0900

    [2025-06-21] 오래된 파일 삭제

--- Assets/Scripts/Creature/PlayerData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Player_Data", menuName = "Scriptable Objects/Player_Data")]
-public class PlayerData : CreatureData
-{
-    public float MP;
-
-    public float inertia;
-}
